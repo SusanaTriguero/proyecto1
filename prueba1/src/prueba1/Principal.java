0da4668e56f449671479fdab9b518c3fa4423743
Admin <Admin@DESKTOP-CH8E6VN>
2021-04-28 09:04:53 +0200

    Creación paquete cambio1 y clase cambio1

--- prueba1/src/prueba1/Principal.java
+++ prueba1/src/prueba1/Principal.java
@@ -3,6 +3,9 @@ package prueba1;
 public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo java-git");
+		System.out.println("Hola mundo java-git");
+		System.out.println("Hola mundo java-git");
+		System.out.println("Pruebas git se añade cambio1");
 	}
 	
 }
